0a20ffaa67da546126558b05034d0d1205b7d647
Ahmed Abou Gabal <78806824+ahmedabougabal@users.noreply.github.com>
2025-04-12 23:59:42 +0200

    feat: Create main.cpp
    adds file

---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+
+using namespace std;
+
+string fizzBuzzAlgo(string phrase)
+{
+  string result = "";
+  if (phrase.empty())
+  {
+    return "";
+  }
+  /*
+  capture words in indices that are divisible by 3 for fizz and 5 for buzz and as for words with indices
+  that are divisble by both 3 and 5 we change it to fizzbuzz
+  */
+  int word_index = 1;
+  string current_word = "";
+  for (int i = 0; i < phrase.size(); ++i)
+  {
+    if (!isspace(phrase[i]))
+    {
+      // this builds up each word one by one
+      current_word += phrase[i];
+    }
+    else
+    {
+      //! process completed words
+      if (word_index % 3 == 0 && word_index % 5 == 0)
+      {
+        result += "fizzbuzz";
+      }
+      else if (word_index % 3 == 0)
+      {
+        result += "fizz";
+      }
+      else if (word_index % 5 == 0)
+      {
+        result += "buzz";
+      }
+      else
+      {
+        result += current_word;
+      }
+      word_index++;
+      current_word = "";
+    }
+    result += phrase[i];
+  }
+  if (!current_word.empty())
+  {
+    if (word_index % 3 == 0 && word_index % 5 == 0)
+    {
+      result += "fizzbuzz";
+    }
+    else if (word_index % 3 == 0)
+    {
+      result += "fizz";
+    }
+    else if (word_index % 5 == 0)
+    {
+      result += "buzz";
+    }
+    else
+    {
+      result += current_word;
+    }
+    word_index++;
+  }
+  return result;
+}
+
+int main()
+{
+  string text = "Mary had a little lamb\n"
+                "Little lamb, little lamb\n"
+                "Mary had a little lamb\n"
+                "It's fleece was white as snow\n";
+  cout << fizzBuzzAlgo(text);
+  return 0;
+}
